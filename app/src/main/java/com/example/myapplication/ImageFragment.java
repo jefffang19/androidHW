1b429592edbdbc8be22c29b1c271577fd581e671
jefffang19 <jefffang07@gmail.com>
2020-07-31 17:29:12 +0800

    create fragment

---
+++ app/src/main/java/com/example/myapplication/ImageFragment.java
@@ -0,0 +1,91 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.io.IOException;
+
+public class ImageFragment extends Fragment {
+    final int ImageRequestCode = 1;
+    final Intent[] intent = new Intent[1];
+
+    ImageView imageview;
+
+    static ImageFragment newInstance(){
+        ImageFragment imageFragment = new ImageFragment();
+        return imageFragment;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //fragment(due to its dynamic nature) can not use finViewById directly
+        //Button button = findViewById(R.id.clickBtn);
+        View view = inflater.inflate(R.layout.show_pic, container, false);
+
+
+        Button clkButton = view.findViewById(R.id.clickBtn);
+        imageview = view.findViewById(R.id.imageView2);
+
+        //if click button, select photo from album
+        clkButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                intent[0] = new Intent(Intent.ACTION_GET_CONTENT);
+                //we want to pick image
+                intent[0].setType("image/*");
+                //pick multiple images
+                //intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+
+                //if need to get result from activity, use startActivityForResult
+                //startActivity(intent);
+                startActivityForResult(intent[0], ImageRequestCode);
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //the result code we want to respond to
+        if(requestCode == ImageRequestCode){
+            Log.d("Activity Result Debug", "album activity return success");
+            //see if request is successful (app didn't crash)
+            if(resultCode == Activity.RESULT_OK){
+                //get our image from the album activity
+                Uri uri = data.getData();
+                Log.d("Activity Result Debug", ""+uri);
+
+                //get image uri as a bitmap
+                Bitmap bitmap = null;
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), uri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if(uri != null){
+                    imageview.setImageBitmap(bitmap);
+                    imageview.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -18,58 +20,17 @@ import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
-    final int ImageRequestCode = 1;
-    final Intent[] intent = new Intent[1];
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //R(created by android) gets data from res folder
 
-        Button button = findViewById(R.id.clickBtn);
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                intent[0] = new Intent(Intent.ACTION_GET_CONTENT);
-                //we want to pick image
-                intent[0].setType("image/*");
-                //pick multiple images
-                //intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-
-                //if need to get result from activity, use startActivityForResult
-                //startActivity(intent);
-                startActivityForResult(intent[0], ImageRequestCode);
-            }
-        });
+        changeFragment(ImageFragment.newInstance());
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        //the result code we want to respond to
-        if(requestCode == ImageRequestCode){
-            Log.d("Activity Result Debug", "album activity return success");
-            //see if request is successful (app didn't crash)
-            if(resultCode == RESULT_OK){
-                //get our image from the album activity
-                Uri uri = data.getData();
-                Log.d("Activity Result Debug", ""+uri);
-
-                //get image uri as a bitmap
-                Bitmap bitmap = null;
-                try {
-                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                if(uri != null){
-                    setContentView(R.layout.show_pic);
-                    ImageView imageView = findViewById(R.id.imageView2);
-                    imageView.setImageBitmap(bitmap);
-                    imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-                }
-            }
-        }
+    public void changeFragment(Fragment f){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fragment_container, f).commit();
     }
+
 }
